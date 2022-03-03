b1ed251ce150ab99d8ec246e539f8c509fdc017e
jenry <>
2022-03-02 22:25:13 -0300

    exercicio3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a distância percorrida em Km: ");
+            decimal.TryParse(Console.ReadLine(), out decimal valorDistancia);
+
+            Console.WriteLine("Informe a quantidade de litros gastos em combustível: "); 
+            decimal.TryParse(Console.ReadLine(), out decimal valorLitros);        
+
+            Console.WriteLine($"Sua média de gasto é: {valorDistancia / valorLitros}");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
